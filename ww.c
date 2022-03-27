4a0736d6d5d4e98442515d2a57d9b09a78dbc2f1
sparikh4889 <100457800+sparikh4889@users.noreply.github.com>
2022-03-27 18:12:22 -0400

    Update ww.c

--- ww.c
+++ ww.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+#include <dirent.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define BUFFER_SIZE 5
 void wrap(int width,int fd_input,int fd_output);
@@ -16,13 +21,43 @@ int main(int argc, char*argv[])
     }
   int width = atoi(argv[1]);
   assert(width > 0);
-
-  if(argc ==2){
-    wrap(width,0,1);
+  
+  // direc
+  if(argc == 2) {
+     char *dirName = argv[2];
+     struct dirent *file;
+     DIR *dir = opendir(dirName);
+     
+     chdir(dirName);
+     int fileLength;
+     
+     if (dir) {
+        while ( (file = readdir(dir)) != NULL) {
+           fileLength = strlen(file->d_name);
+           char *fileName = malloc(sizeof(char) * fileLength + 1);
+           strcpy(fileName, file->d_name);
+           
+           if ( !((strncmp(".", fileName, 1) == 0) || (strncmp("wrap.", fileName, 5) == 0)) ) {
+              int fd = open(fileName, O_RDONLY);
+              char *wrapped = malloc(sizeof(char) * 6 + fileLength);
+              strcpy(wrapped, "wrap.");
+              strcat(wrapped, fileName);
+              
+              int newfd = open(wrapped, O_WRONLY | O_TRUNC | O_CREAT, 0666);
+              wrap(width, fd, newfd);
+              free(wrapped);
+              close(fd);
+              close(newfd);
+           }
+           free(fileName);
+        }
+        closedir(dir);
+     }
+    // wrap(width,0,1);
   }
   else{
     int f = isFileOrDir(argv[2]);
-
+      // file
       if (f == 1) {
         int fd = open(argv[2], O_RDONLY);
         if(fd < 0){
